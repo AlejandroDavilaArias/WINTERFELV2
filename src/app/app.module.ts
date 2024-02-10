696c7b32edd8e5b2c8616be449a3a54553a9e0c8
John Alejandro Davila Arias <davil451998@gmail.com>
2024-02-10 17:45:21 -0500

    v1.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+//Modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+//Componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegadorComponent } from './navegador/navegador.component';
+import { ListaTareasComponent } from './lista-tareas/lista-tareas.component';
+import { AgregarTareaComponent } from './agregar-tarea/agregar-tarea.component';
+import { InicioSeccionComponent } from './inicio-seccion/inicio-seccion.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
+import { NotasComponent } from './notas/notas.component';
+import { AlertaComponent } from './alerta/alerta.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavegadorComponent, ListaTareasComponent, AgregarTareaComponent, InicioSeccionComponent, TarjetaComponent, NotasComponent, AlertaComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/inicio-seccion/inicio-seccion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-seccion',
+  templateUrl: './inicio-seccion.component.html',
+  styleUrl: './inicio-seccion.component.css'
+})
+export class InicioSeccionComponent {
+
+}
