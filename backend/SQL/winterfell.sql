4c9ed4d2213641e5240b65d6b76307019e1cf37d
AlejandroDavilaArias <davil451998@gmail.com>
2024-07-11 10:20:57 -0500

    servidor

--- backend/SQL/winterfell.sql
+++ backend/SQL/winterfell.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-05-2024 a las 00:11:52
+-- Tiempo de generación: 11-07-2024 a las 16:03:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,53 +24,92 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `notas`
+-- Estructura de tabla para la tabla `cita`
 --
 
-CREATE TABLE `notas` (
-  `id_nota` int(11) NOT NULL,
-  `fecha` time DEFAULT NULL,
-  `hora` time DEFAULT NULL,
-  `importancia` tinyint(1) DEFAULT NULL,
-  `nota` varchar(500) DEFAULT NULL,
-  `fo_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='creación de la nota del usuario';
+CREATE TABLE `cita` (
+  `id_citas` int(10) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `nota` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cita`
+--
+
+INSERT INTO `cita` (`id_citas`, `fecha`, `hora`, `nota`) VALUES
+(16, '2024-07-10', '07:00:00', 'cita prueba'),
+(19, '2024-07-10', '15:52:00', 'cita 2'),
+(49, '0000-00-00', '00:00:00', ''),
+(50, '0000-00-00', '00:00:00', ''),
+(51, '2024-07-10', '16:36:00', 'Nota de prueba'),
+(52, '0000-00-00', '00:00:00', '');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `notas`
 --
 
-CREATE TABLE `usuarios` (
+CREATE TABLE `notas` (
+  `id_nota` int(12) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time DEFAULT NULL,
+  `nota` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='creación de la nota del usuario';
+
+--
+-- Volcado de datos para la tabla `notas`
+--
+
+INSERT INTO `notas` (`id_nota`, `fecha`, `hora`, `nota`) VALUES
+(0, '2024-07-10', '14:30:00', 'Nota prueba'),
+(1, '2024-06-13', '22:03:59', 'Nota 1'),
+(80, '2024-06-19', '20:02:00', 'Nota 2'),
+(87, '2024-06-12', '23:00:00', 'nota 3'),
+(95, '2024-06-21', '02:00:00', 'Nota 4');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
-  `nombre` varchar(200) DEFAULT NULL,
-  `correo` varchar(200) DEFAULT NULL,
-  `contrasena` varchar(100) DEFAULT NULL
+  `nombre` varchar(1000) NOT NULL,
+  `email` varchar(2000) NOT NULL,
+  `clave` varchar(2500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre`, `correo`, `contrasena`) VALUES
-(1, 'prueba', 'prueba1@hotmail.com', 'prueba1');
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `clave`) VALUES
+(1, 'alejo', 'alejo@email.com', '12345');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD PRIMARY KEY (`id_citas`);
+
+--
 -- Indices de la tabla `notas`
 --
 ALTER TABLE `notas`
-  ADD PRIMARY KEY (`id_nota`),
-  ADD KEY `fo_usuario` (`fo_usuario`);
+  ADD PRIMARY KEY (`id_nota`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `usuario`
 --
-ALTER TABLE `usuarios`
+ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`);
 
 --
@@ -78,10 +117,16 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `cita`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `cita`
+  MODIFY `id_citas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
